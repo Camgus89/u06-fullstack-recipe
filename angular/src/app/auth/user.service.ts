f96e438b08f5560e7d6ebf23e36db3bcd7085d1e
Camilla Gustafsson <camie-89@hotmail.com>
2023-09-12 14:18:22 +0200

    .

--- angular/src/app/auth/user.service.ts
+++ angular/src/app/auth/user.service.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class UserService {
-  configURL = "http://u06-fullstack-recipe-production.up.railway.app";
+  configURL = "https://u06-fullstack-recipe-production.up.railway.app/";
 
   httpOptions = {
     headers: new HttpHeaders({
